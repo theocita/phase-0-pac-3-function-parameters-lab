5ea2646c6037e63d592932af7223e918aa65c826
Theocita <theocitaallen@gmail.com>
2022-07-05 20:57:13 -0400

    Complete assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(name) {
+    console.log(name)
+    return "Hi, my name is ${name}.";
+}
+function introductionWithLanguages(name, languages) {
+    console.log (name, language);
+    return "Hi, my name is ${name} and I am learning to program in ${language}";
+}
+function introductionWithLanguagesOptional(name, lamguage = "Javascript") {
+console.log (name, language);
+    return "Hi, my name is ${name} and I am learning to program in ${language}";
+}
